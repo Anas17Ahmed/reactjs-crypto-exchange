ee7fb788bc36940d6e767d61c99c10b72c742ee3
anas17ahmed <anas17ahmed@gmail.com>
2023-05-29 16:45:55 +0500

    Assignment 2

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(JSON.parse(localStorage.getItem('blogs')|| '[]'));
   const [modalOpen, setModalOpen] = useState(false);
+  const [selectedBlog, setSelectedBlog] = useState(null);
   const [newBlog, setNewBlog] = useState({
     title: '',
     subtitle: '',
@@ -11,7 +12,7 @@ const Blogs = () => {
 
   useEffect(() => {
     // Retrieve blogs from local storage
-    const storedBlogs = JSON.parse(localStorage.getItem('blogs'));
+    const storedBlogs = JSON.parse(localStorage.getItem('blogs')|| '[]');
     if (storedBlogs) {
       setBlogs(storedBlogs);
     }
@@ -43,25 +44,54 @@ const Blogs = () => {
   };
 
   const handleViewBlog = (blogId) => {
-    // Handle view action
-    console.log(`View blog with ID: ${blogId}`);
+    const blog = blogs.find((blog) => blog.id === blogId);
+    setSelectedBlog(blog);
+    setModalOpen('view');
   };
 
   const handleEditBlog = (blogId) => {
-    // Handle edit action
-    console.log(`Edit blog with ID: ${blogId}`);
+    const blog = blogs.find((blog) => blog.id === blogId);
+    setSelectedBlog(blog);
+    setNewBlog({
+      title: blog.title,
+      subtitle: blog.subtitle,
+      author: blog.author,
+    });
+    setModalOpen('edit');
+  };
+
+  const handleUpdateBlog = () => {
+    setBlogs((prevBlogs) => {
+      const updatedBlogs = prevBlogs.map((blog) => {
+        if (blog.id === selectedBlog.id) {
+          return {
+            ...blog,
+            title: newBlog.title,
+            subtitle: newBlog.subtitle,
+            author: newBlog.author,
+          };
+        }
+        return blog;
+      });
+      return updatedBlogs;
+    });
+    setNewBlog({
+      title: '',
+      subtitle: '',
+      author: '',
+    });
+    setSelectedBlog(null);
+    setModalOpen(false);
   };
 
   const handleDeleteBlog = (blogId) => {
-    // Handle delete action
-    console.log(`Delete blog with ID: ${blogId}`);
     setBlogs((prevBlogs) => prevBlogs.filter((blog) => blog.id !== blogId));
   };
 
   return (
     <div className='container'>
-      <h2 className='m-3 text-center'>Blogs</h2>
-      <button className="btn btn-primary mb-3" onClick={() => setModalOpen(true)}>+ Add</button>
+      <h2 className='text-center'>Blogs</h2>
+      <button className="btn btn-primary my-3" onClick={() => setModalOpen('add')}>+ Add</button>
       <table className="table">
         <thead>
           <tr>
@@ -89,36 +119,72 @@ const Blogs = () => {
         </tbody>
       </table>
 
-      <div className={`modal ${modalOpen ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: modalOpen ? 'block' : 'none' }}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Add New Blog</h5>
-              <button type="button" className="close" onClick={() => setModalOpen(false)}>
-                <span aria-hidden="true">&times;</span>
-              </button>
+      {(modalOpen == 'edit' || modalOpen == 'add') && (
+        <div className={`modal ${modalOpen ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: modalOpen ? 'block' : 'none' }}>
+            <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                {modalOpen === 'edit' ? (
+                    <h5 className="modal-title">Edit Blog</h5>
+                ) : (
+                    <h5 className="modal-title">Add New Blog</h5>
+                )}
+                <button type="button" className="close" onClick={() => setModalOpen(false)}>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div className="modal-body">
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" name="title" value={newBlog.title} onChange={handleInputChange} />
+                </div>
+                <div className="form-group">
+                    <label>Subtitle</label>
+                    <input type="text" className="form-control" name="subtitle" value={newBlog.subtitle} onChange={handleInputChange} />
+                </div>
+                <div className="form-group">
+                    <label>Author</label>
+                    <input type="text" className="form-control" name="author" value={newBlog.author} onChange={handleInputChange} />
+                </div>
+                </div>
+                <div className="modal-footer">
+                {modalOpen === 'edit' ? (
+                    <button type="button" className="btn btn-primary" onClick={handleUpdateBlog}>Update</button>
+                ) : (
+                    <button type="button" className="btn btn-primary" onClick={handleAddBlog}>Save</button>
+                )}
+                <button type="button" className="btn btn-secondary" onClick={() => setModalOpen(false)}>Cancel</button>
+                </div>
             </div>
-            <div className="modal-body">
-              <div className="form-group">
-                <label>Title</label>
-                <input type="text" className="form-control" name="title" value={newBlog.title} onChange={handleInputChange} />
-              </div>
-              <div className="form-group">
-                <label>Subtitle</label>
-                <input type="text" className="form-control" name="subtitle" value={newBlog.subtitle} onChange={handleInputChange} />
-              </div>
-              <div className="form-group">
-                <label>Author</label>
-                <input type="text" className="form-control" name="author" value={newBlog.author} onChange={handleInputChange} />
-              </div>
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-primary" onClick={handleAddBlog}>Save</button>
-              <button type="button" className="btn btn-secondary" onClick={() => setModalOpen(false)}>Cancel</button>
+        </div>
+      )}
+
+      {selectedBlog && modalOpen == 'view' && (
+        <div className={`modal ${modalOpen ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: modalOpen ? 'block' : 'none' }}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">View Blog</h5>
+                <button type="button" className="close" onClick={() => setModalOpen(false)}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <h6>Title:</h6>
+                <p>{selectedBlog.title}</p>
+                <h6>Subtitle:</h6>
+                <p>{selectedBlog.subtitle}</p>
+                <h6>Author:</h6>
+                <p>{selectedBlog.author}</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setModalOpen(false)}>Close</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
